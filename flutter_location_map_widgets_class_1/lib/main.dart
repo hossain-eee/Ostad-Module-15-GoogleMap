ccf896ddbfa60f44bda2b0fef6e1515e28b38762
Ismail Hossain <hossain.eee042@gmail.com>
2023-08-27 12:39:57 +0600

    GoogleMap() widget use for get location byusing lat lang

--- flutter_location_map_widgets_class_1/lib/main.dart
+++ flutter_location_map_widgets_class_1/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+//api key: AIzaSyB7w8JhkAHJJkhweXYfp7w_28JRYI6o8zg
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -24,9 +27,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text("Google Map Screen"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Map Screen"),
+      ),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(24.605169371098192, 89.64165685434453),
+        ),
+      ), //Tarakandi school lat and lang
     );
   }
 }
-
