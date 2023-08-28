442c56c19495172e16fb8dbed15aa558001e6f92
Ismail Hossain <hossain.eee042@gmail.com>
2023-08-28 14:30:46 +0600

    listen user location  movement or track user movemeent

--- flutter_location_map_widgets_class_3_gps/lib/main.dart
+++ flutter_location_map_widgets_class_3_gps/lib/main.dart
@@ -44,6 +44,19 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  //Listen to my location that mean track user activity
+  void listenToMyLocation() async{
+    Location.instance.onLocationChanged.listen((location) {
+      myCurrentLocation=location; //myCurrentLocation got latest data from listen
+      print('listening to location $location');
+      if(mounted){
+        setState(() {
+
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,12 +74,27 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       //button for get location
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //get current location
-          getMyLocation();
-        },
-        child: Icon(Icons.my_location),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+
+          FloatingActionButton(
+            onPressed: () {
+              //get latest location by listen user activity
+              listenToMyLocation() ;
+            },
+            child: Icon(Icons.location_on),
+          ),
+          SizedBox(width: 20,),
+          FloatingActionButton(
+            onPressed: () {
+              //get current location
+              getMyLocation();
+            },
+            child: Icon(Icons.my_location),
+          ),
+          SizedBox(width: 20,),
+        ],
       ),
     );
   }
