e0f650ac7a4b102a12dd86d739a63cb73772daa9
Ismail Hossain <hossain.eee042@gmail.com>
2023-08-28 14:11:38 +0600

    request for user permission and check the permission status

--- flutter_location_map_widgets_class_3_gps/lib/main.dart
+++ flutter_location_map_widgets_class_3_gps/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+
 void main() {
   runApp(const GPSLocationApp());
 }
+
 class GPSLocationApp extends StatelessWidget {
   const GPSLocationApp({super.key});
 
@@ -25,32 +27,47 @@ class _HomeScreenState extends State<HomeScreen> {
   LocationData? myCurrentLocation;
 
   //get current location
-  void getMyLocation() async{
-     myCurrentLocation = await Location.instance.getLocation();
+  void getMyLocation() async {
+    //get the permission
+    await Location.instance.requestPermission().then((requestPermission) {
+      print("Request to the user permission: $requestPermission");
+    });
+    //check the permission is available or not (status)
+    await Location.instance.hasPermission().then((permissionStatus) {
+      print("Is user give permission : $permissionStatus");
+    });
+    //fetch the current location
+    myCurrentLocation = await Location.instance.getLocation();
     print("My current Location is : $myCurrentLocation");
-    if(mounted){
-      setState(() {
-
-      });
+    if (mounted) {
+      setState(() {});
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("GPS Location"),),
-      body: Column(
-        children: [
-          Text("My Location"),
-          Text('${myCurrentLocation?.latitude ?? ''}'
-              '${myCurrentLocation?.longitude ?? ''}'),
-        ],
+      appBar: AppBar(
+        title: const Text("GPS Location"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("My Location"),
+            Text('Lat-${myCurrentLocation?.latitude ?? ''}  '
+                'Lon-${myCurrentLocation?.longitude ?? ''}'),
+          ],
+        ),
       ),
       //button for get location
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        //get current location
-        getMyLocation();
-      }, child: Icon(Icons.my_location),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //get current location
+          getMyLocation();
+        },
+        child: Icon(Icons.my_location),
+      ),
     );
   }
 }
-
