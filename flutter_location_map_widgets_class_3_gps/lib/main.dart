092fecf240ce9bb57a1412139fc340d26921d22c
Ismail Hossain <hossain.eee042@gmail.com>
2023-08-28 13:37:24 +0600

    current location fetching

--- flutter_location_map_widgets_class_3_gps/lib/main.dart
+++ flutter_location_map_widgets_class_3_gps/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:location/location.dart';
 void main() {
   runApp(const GPSLocationApp());
 }
@@ -22,10 +22,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  LocationData? myCurrentLocation;
+
+  //get current location
+  void getMyLocation() async{
+     myCurrentLocation = await Location.instance.getLocation();
+    print("My current Location is : $myCurrentLocation");
+    if(mounted){
+      setState(() {
+
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("GPS Location"),),
+      body: Column(
+        children: [
+          Text("My Location"),
+          Text('${myCurrentLocation?.latitude ?? ''}'
+              '${myCurrentLocation?.longitude ?? ''}'),
+        ],
+      ),
+      //button for get location
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        //get current location
+        getMyLocation();
+      }, child: Icon(Icons.my_location),),
     );
   }
 }
