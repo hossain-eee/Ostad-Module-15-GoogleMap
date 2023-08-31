9956f64a4e16da3b364f8e7862c1cfb294138817
Ismail Hossain <hossain.eee042@gmail.com>
2023-08-31 20:58:02 +0600

    user current location fetch

--- how_to_move_markers_position_smoothly/lib/main.dart
+++ how_to_move_markers_position_smoothly/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,9 +24,12 @@ class _HomeState extends State<Home> {
   GoogleMapController? mapController;
   Set<Marker> markers = Set();
   Set<Polyline> polylines = {};
-  LatLng initialLocation = LatLng(27.6602292, 85.308027);
-  LatLng movingLocation = LatLng(27.6602292, 85.308027);
+  // LatLng initialLocation = LatLng(27.6602292, 85.308027);
+  LatLng initialLocation = LatLng(0.0,0.0);
+  // LatLng movingLocation = LatLng(27.6602292, 85.308027);
+  LatLng movingLocation = LatLng(0.0,0.0);
   List<LatLng> poly_points = [];
+  Location location = Location();
   // Number of steps for moving marker
   int numSteps = 50;
   int delayMilliseconds = 50;
@@ -36,10 +40,62 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-    poly_points.add(initialLocation);
-    currentPosition = [initialLocation.latitude, initialLocation.longitude];
-    addMarkers();
+    
+    
+    _getCurrentLocation();
+    // addMarkers();
     super.initState();
+  }
+
+  //get user location
+  Future<void> _getCurrentLocation() async {
+    //this permission part is taken from location package (readme)
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    try {
+       Location.instance.changeSettings(
+      distanceFilter: 5, //meter
+      // accuracy:LocationAccuracy.navigation, // walking track
+      accuracy: LocationAccuracy.high,
+      interval: 3000, // 3 second
+    );
+      LocationData locationData = await location.getLocation();
+      initialLocation = LatLng(locationData.latitude!, locationData.longitude!);
+      movingLocation = LatLng(locationData.latitude!, locationData.longitude!);
+      /*    location_move_marker =
+          LatLng(locationData.latitude!, locationData.longitude!); */
+
+      addMarkers();
+      currentPosition = [initialLocation.latitude, initialLocation.longitude];
+      poly_points.add(movingLocation);//initial first location or starting point of polyline
+      
+      // camera focus on initial location
+      mapController!.animateCamera(CameraUpdate.newLatLng(
+          LatLng(locationData.latitude!, locationData.longitude!)));
+      print("Location 1: $initialLocation");
+      print("Location 2: $movingLocation");
+      setState(() {});
+    } catch (error) {
+      print("Error getting location: $error");
+    }
   }
 
   addMarkers() {
@@ -72,7 +128,7 @@ class _HomeState extends State<Home> {
 
   moveMarkerToLocation(LatLng targetLocation) {
     movingLocation = targetLocation;
-    
+
 /* This calculates how much the marker's latitude (up and down on the map) 
 should change in each step to reach the target location. 
 It takes the difference between the target latitude and the current latitude, 
@@ -89,7 +145,10 @@ then divides it by the total number of steps. */
     /* The current latitude of the marker is updated by adding the calculated latitude step. */
     currentPosition[0] += stepLat!; // let 24.5968 + 0.5,
     currentPosition[1] += stepLng!; // // let 89.5968 + 0.3
-  poly_points.add(LatLng(currentPosition[0], currentPosition[1])); // get latlng from user selected point via currentPosition
+    poly_points.add(LatLng(
+        currentPosition[0],
+        currentPosition[
+            1])); // get latlng from user selected point via currentPosition
 
     /* A new LatLng object is created using the updated latitude and longitude. */
     var newPosition = LatLng(currentPosition[0], currentPosition[1]);
